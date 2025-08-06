cb41d575628c2f27548869f012f09b079994451d
sayan4545 <sayanchatterjee3010@gmail.com>
2025-08-07 03:40:04 +0530

    m,ap

--- src/main/java/com/dev/reactive/streams/StreamDemo02.java
+++ src/main/java/com/dev/reactive/streams/StreamDemo02.java
@@ -18,6 +18,14 @@ public class StreamDemo02 {
             if(n%2==0) System.out.print(n+" ");
         }
         System.out.println();
-        Arrays.stream(nums).filter(x->x%2==0).forEach(System.out::println);
+        Arrays.stream(nums)
+                .peek(x-> System.out.println("processing: "+x))
+                .filter(x->x>5)
+                .forEach(System.out::println);
+
+        List<Integer> l1 = List.of(1,2,3,4,5,6,7,8,9,10);
+        List<Integer> streamMap = l1.stream().map(x -> x * 2)
+                .toList();
+        streamMap.forEach(System.out::println);
     }
 }
